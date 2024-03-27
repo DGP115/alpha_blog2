f476f770137fe1895a319df46b9628975b9f7e45
DGP115 <david.g.peloso@gmail.com>
2024-03-27 14:25:10 -0400

    User model associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_27_153818) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_27_180938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_27_153818) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
