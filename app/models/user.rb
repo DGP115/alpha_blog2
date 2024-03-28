ee67d71a19fe49eaf7a228a6c95d11e53bf8948b
DGP115 <david.g.peloso@gmail.com>
2024-03-27 21:26:18 -0400

    Add before_save to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Class to model User of Alpha Blog 2
 class User < ApplicationRecord
   # Validations
+  before_save { self.email_address = email_address.downcase }
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { maximum: 100 }
@@ -13,4 +14,5 @@ class User < ApplicationRecord
 
   # Associations
   has_many :articles, dependent: :destroy
+
 end
